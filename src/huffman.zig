da7e8dc13db6a60d4eef9fea5b6b01ed772ae203
Ben Russell <thematrixeatsyou@gmail.com>
2020-05-20 19:44:30 +1200

    Calculate CanonicalHuffmanTree().Tkey + cleanup

--- src/huffman.zig
+++ src/huffman.zig
@@ -13,9 +13,13 @@ pub fn CanonicalHuffmanTree(comptime Tlen: type, max_len: usize) type {
                 .bits = @floatToInt(comptime_int, @ceil(@log2(@intToFloat(f64, max_len)))),
             },
         });
-        const Tkey: type = usize;
         const bit_width_count: usize = (1 << @typeInfo(Tlen).Int.bits);
-        const Tmask: type = usize;
+        pub const Tkey: type = @Type(TypeInfo{
+            .Int = .{
+                .is_signed = false,
+                .bits = bit_width_count,
+            },
+        });
 
         // Number of symbols that are actually in the tree.
         symbol_count: Tkey,
@@ -25,20 +29,20 @@ pub fn CanonicalHuffmanTree(comptime Tlen: type, max_len: usize) type {
         symbol_tree_raw: [max_len]Tval, // backing buffer for symbol_tree
 
         // Map: bit width -> value past last possible value for the bit width.
-        symbol_ends: [bit_width_count]Tmask,
+        symbol_ends: [bit_width_count]Tkey,
 
         // Map: bit width -> index into symbol_tree to first entry of the given bit width.
         symbol_offsets: [bit_width_count]Tkey,
 
         pub fn fromLengths(lengths: []const Tlen) Self {
-            const symbol_count = lengths.len;
+            const symbol_count = @intCast(Tkey, lengths.len);
 
             // Sort the symbols in length order, ignoring 0-length
             var symbol_tree = [1]Tval{0} ** max_len;
             var symbol_ends = [1]Tkey{0} ** bit_width_count;
             var symbol_offsets = [1]Tkey{0} ** bit_width_count;
-            var nonzero_count: usize = 0;
-            var end_value: usize = 0;
+            var nonzero_count: Tkey = 0;
+            var end_value: Tkey = 0;
             {
                 var bit_width: usize = 1;
                 while (bit_width < bit_width_count) : (bit_width += 1) {
